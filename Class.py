27852d655ee6cdd91e579eace8132a4f820698a6
Banda Subagja <67062360+bandasubagja@users.noreply.github.com>
2021-03-05 14:27:55 +0700

    1904692

--- Class.py
+++ Class.py
@@ -1,22 +1,18 @@
 class student():
-    
-    def nama(self):
-        print("Nama   :", self.full_name)
-        
-    def get_age (self):
-        print("Usia   :", self.age)
-    def get_alamat (self):
-        print("Alamat :", self.alamat)
-    def get_status (self):
-        print("Status :", self.status)
-budi = student()
+    def __init__(self, a,b,c,d):
+        self.full_name = a
+        self.age = b
+        self.alamat = c
+        self.status = d
+    def get_age(self):
+        return self.age
+    def get_alamat(self):
+        return self.alamat
+    def get_status(self):
+        return self.status
 
-budi.full_name = "Budi Permana"
-budi.age = "21"
-budi.alamat = "Bandung"
-budi.status = "Mahasiswa"
-
-budi.nama()
-budi.get_age()
-budi.get_alamat()
-budi.get_status()
+f = student("Banda Subaja", 20, "Bandung", "Mahasiswa")
+print("Nama   : ", f.full_name)
+print("Usia   : ", f.get_age())
+print("Alamat : ", f.get_alamat())
+print("Status : ", f.get_status())
