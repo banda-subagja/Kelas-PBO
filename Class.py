d999eb6872df8f5a3120e6ed6ea07dc4171de0b8
Banda Subagja <bandasubagja@gmail.com>
2021-03-05 11:34:46 +0700

    1904692

---
+++ Class.py
@@ -0,0 +1,22 @@
+class student():
+    
+    def nama(self):
+        print("Nama :", self.full_name)
+        
+    def get_age (self):
+        print("Usia :", self.age)
+    def get_alamat (self):
+        print("Usia :", self.alamat)
+    def get_status (self):
+        print("Usia :", self.status)
+budi = student()
+
+budi.full_name = "Budi Permana"
+budi.age = "21"
+budi.alamat = "Bandung"
+budi.status = "Mahasiswa"
+
+budi.nama()
+budi.get_age()
+budi.get_alamat()
+budi.get_status()
